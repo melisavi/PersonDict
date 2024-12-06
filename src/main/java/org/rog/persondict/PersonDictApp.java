4b689f929e301be5fa475dfbcbbf1b640c826f2e
Elena Rog <rog.elena.yu@gmail.com>
2024-12-07 00:38:34 +0300

    model, db creation

--- src/main/java/org/rog/persondict/PersonDictApp.java
+++ src/main/java/org/rog/persondict/PersonDictApp.java
@@ -1,2 +1,11 @@
-package org.rog.persondict;public class PersonDictApp {
+package org.rog.persondict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonDictApp {
+    public static void main(String[] args){
+        SpringApplication.run(PersonDictApp.class);
+    }
 }
